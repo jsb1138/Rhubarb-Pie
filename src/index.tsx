c96b37c48df984eeab44413828825255967501a3
Joel Boychuk <joelboychuk@gmail.com>
2022-11-09 18:50:02 +0100

    ionic build, bg + buttons

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { StatusBar, Style } from '@capacitor/status-bar';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
+
+// iOS only
+window.addEventListener('statusTap', function () {
+  console.log('statusbar tapped');
+});
+
+// Display content under transparent status bar (Android only)
+StatusBar.setOverlaysWebView({ overlay: true });
+
+const setStatusBarStyleDark = async () => {
+  await StatusBar.setStyle({ style: Style.Dark });
+};
+
+const setStatusBarStyleLight = async () => {
+  await StatusBar.setStyle({ style: Style.Light });
+};
+
+const hideStatusBar = async () => {
+  await StatusBar.hide();
+};
+
+const showStatusBar = async () => {
+  await StatusBar.show();
+};
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
