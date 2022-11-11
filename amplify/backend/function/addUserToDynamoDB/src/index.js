8df87e296177f4a6222ee88f2d81463e7d565b50
Joel Boychuk <joelboychuk@gmail.com>
2022-11-12 00:34:21 +0100

    cleaned up lambda function and DB

--- amplify/backend/function/addUserToDynamoDB/src/index.js
+++ amplify/backend/function/addUserToDynamoDB/src/index.js
@@ -1,5 +1,5 @@
-var aws = require("aws-sdk");
-var ddb = new aws.DynamoDB();
+let aws = require("aws-sdk");
+const ddb = new aws.DynamoDB();
 
 exports.handler = async (event, context) => {
   let date = new Date();
@@ -9,27 +9,23 @@ exports.handler = async (event, context) => {
       Item: {
         id: { S: event.request.userAttributes.sub },
         __typename: { S: "User" },
-        name: { S: event.request.userAttributes.name },
+        username: { S: event.userName },
         email: { S: event.request.userAttributes.email },
+        favorites: { L: [] },
         createdAt: { S: date.toISOString() },
         updatedAt: { S: date.toISOString() },
       },
       TableName: process.env.USERTABLE,
     };
-
-    // Call DynamoDB
     try {
       await ddb.putItem(params).promise();
-      console.log("Success");
+      console.log("Success!");
     } catch (err) {
       console.log("Error", err);
     }
-
-    console.log("Success: Everything executed correctly");
     context.done(null, event);
   } else {
-    // Nothing to do, the user's email ID is unknown
-    console.log("Error: Nothing was written to DynamoDB");
+    console.log("Error: Nothing was written to the DB");
     context.done(null, event);
   }
 };
