c2ed05fa28b09b6778d1afee89fe3a8ac396e21d
Joel Boychuk <joelboychuk@gmail.com>
2022-11-13 15:49:21 +0100

    tightened authorization on db queries

--- src/App.js
+++ src/App.js
@@ -1,71 +1,104 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-import { API, graphqlOperation } from "aws-amplify";
+import { API } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import { getPie, getSlice } from "./graphql/queries";
 import * as queries from "./graphql/queries";
-
-const grabUser = async (userId) => {
-  try {
-    console.log("this is the user favorite:", userId.favorites);
-    // const res = await API.graphql(graphqlOperation(getUser, { id: user }));
-    const res = await API.graphql({
-      query: queries.listUsers,
-      // variables: { id: userId },
-      authMode: "AMAZON_COGNITO_USER_POOLS",
-    });
-    const data = res.data;
-    console.log("user: ", data);
-  } catch (err) {
-    console.error("ERROR: ", err);
-  }
-};
-
-const grabPie = async () => {
-  try {
-    const res = await API.graphql(
-      graphqlOperation(getPie, { id: "01_launch_2022" })
-    );
-    const data = res.data;
-    console.log("pie: ", data);
-  } catch (err) {
-    console.error("ERROR: ", err);
-  }
-};
-
-const grabSlice = async () => {
-  try {
-    const res = await API.graphql(graphqlOperation(getSlice, { id: "1R_1" }));
-    const data = res.data;
-    console.log("slices: ", data);
-  } catch (err) {
-    console.error("ERROR: ", err);
-  }
-};
+import { useState, useEffect } from "react";
+import SliceItem from "./components/SliceItem";
+import PieItem from "./components/PieItem";
 
 function App({ signOut, user }) {
-  console.log("USER-->", user);
-  console.log("USER SUB-->", user.attributes.sub);
+  const [allPies, setAllPies] = useState([]);
+  const [allSlices, setAllSlices] = useState([]);
+
+  const userId = user.attributes.sub;
+
+  const grabUser = async (userId) => {
+    try {
+      const res = await API.graphql({
+        query: queries.listUsers,
+        // variables: { id: userId },
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+      });
+      const data = res.data;
+      // console.log("user faves:", data.listUsers.items[0].favorites);
+      console.log("user: ", data);
+    } catch (err) {
+      console.error("ERROR: ", err);
+    }
+  };
+
+  const grabPies = async () => {
+    try {
+      const res = await API.graphql({
+        query: queries.listPies,
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+      });
+      const data = await res.data.listPies.items;
+      // setAllPies(data);
+      return data;
+    } catch (err) {
+      console.error("ERROR: ", err);
+    }
+  };
+
+  const grabSlice = async () => {
+    try {
+      const res = await API.graphql({
+        query: queries.getSlice,
+        variables: { id: "1R_1" },
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+      });
+      // const res = await API.graphql(graphqlOperation(getSlice, { id: "1R_1" }));
+      const data = res.data;
+      console.log("slice: ", data);
+    } catch (err) {
+      console.error("ERROR: ", err);
+    }
+  };
+
+  const grabSlices = async () => {
+    try {
+      const res = await API.graphql({
+        query: queries.listSlices,
+        // variables: { id: userId },
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+      });
+      // const res = await API.graphql(graphqlOperation(listSlices));
+      const data = await res.data.listSlices.items;
+      // setAllSlices(data);
+      return data;
+    } catch (err) {
+      console.error("ERROR: ", err);
+    }
+  };
+
+  useEffect(() => {
+    const dataFetch = [grabPies(), grabSlices()];
+    Promise.all(dataFetch).then((result) => {
+      setAllPies(result[0]);
+      setAllSlices(result[1]);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <button onClick={() => grabUser(user.attributes.sub)}>User</button>
-      <button onClick={grabPie}>Pie</button>
+      {allPies.map((pie) => (
+        <PieItem key={pie.id} pie={pie} />
+      ))}
+      {/* {allSlices
+        // .filter((slice) => slice.id)
+        .map((slice) => (
+          <SliceItem key={slice.id} slice={slice} />
+        ))} */}
+      {allSlices
+        .filter((slice) => allPies[0].slices.includes(slice.id))
+        .map((slice) => (
+          <SliceItem key={slice.id} slice={slice} />
+        ))}
+      <button onClick={() => grabUser(userId)}>User</button>
+      <button onClick={() => console.log("pies:", allPies)}>Pie</button>
       <button onClick={grabSlice}>Slice</button>
       <button onClick={signOut}>SIGN OUT</button>
     </div>
---
+++ src/components/SliceItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SliceItem() {
+  return (
+    <div>This is a slice!</div>
+  )
+}
