b65ae23ce19f209f234bf810ac3a65a2d8993057
Joel Boychuk <joelboychuk@gmail.com>
2022-11-16 00:53:41 +0100

    slice player taken care of

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,5 +1,6 @@
 import { useAudioPlayer } from "react-use-audio-player";
 import React, { useState } from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 // export default function Player() {
 const AudioPlayer = ({ file }) => {
@@ -20,69 +21,47 @@ const AudioPlayer = ({ file }) => {
     onend: () => console.log("sound has ended!"),
   });
 
-  // const sound = new Howl({
-  //   src: ["http://www.thefarlife.com/saw3.wav"],
-  //   html5: true,
-  // });
-  // let checkRate = 500;
-  // let fadeRate = 5000;
-
-  // function playHandler() {
-  //   if (isPlaying) {
-  //     console.log("paused");
-  //     sound.fade(1, 0, 200);
-  //     setTimeout(() => {
-  //       setIsPlaying(false);
-  //       sound.pause();
-  //     }, 201);
-  //   } else {
-  //     console.log("played");
-  //     setIsPlaying(true);
-  //     sound.play();
-  //     sound.fade(0, 1, 400);
-  //   }
-  // }
-
-  // function soundFadeout() {
-  //   sound.fade(1, 0, fadeRate);
-  //   setIsFading(true);
-  //   setTimeout(() => {
-  //     sound.stop();
-  //     setIsPlaying(false);
-  //     setIsFading(false);
-  //   }, fadeRate);
-  // }
-
-  // if (isPlaying) {
-  //   const progressCheck = setInterval(() => {
-  //     // progress = sound.seek();
-  //     if (sound.seek() > 3) {
-  //       checkRate = null;
-  //       clearInterval(progressCheck);
-  //       soundFadeout();
-  //     }
-  //   }, checkRate);
-  // }
-
-  // function prog() {
-  //   if (isPlaying) {
-  //     const checkTime = setInterval(() => {
-  //       progress = sound.seek();
-  //     }, 100);
-  //   }
-  //   return progress;
-  // }
+  const nudge = {
+    paddingRight: "-40px",
+  };
 
   return (
     <>
-      <button id="play-btn" onClick={togglePlayPause}>
-        <div>
-          <h2>{!isPlaying ? "►" : "▐▐"}</h2>
+      <button
+        id="play-btn"
+        onClick={() => {
+          togglePlayPause();
+          if (!isPlaying) {
+            setIsPlaying(true);
+          } else {
+            setIsPlaying(false);
+          }
+        }}
+      >
+        <div className="center">
+          <div className={!isPlaying ? "play-container" : "pause-container"}>
+            <h2>{!isPlaying ? "►" : "▐▐"}</h2>
+          </div>
+          <div className={!isPlaying ? "countdown1" : "countdown2"}>
+            {!isPlaying ? (
+              <CountdownCircleTimer
+                duration={60}
+                colors={["#1a1835", "#da01f7", "#a30067", "#A30000"]}
+                colorsTime={[10, 6, 4, 0]}
+              ></CountdownCircleTimer>
+            ) : (
+              <CountdownCircleTimer
+                isPlaying
+                duration={100}
+                colors={["#1a1835", "#da01f7", "#a30067", "#A30000"]}
+                colorsTime={[10, 6, 4, 0]}
+              ></CountdownCircleTimer>
+            )}
+          </div>
         </div>
       </button>
     </>
   );
 };
-// }
 
 export default AudioPlayer;
